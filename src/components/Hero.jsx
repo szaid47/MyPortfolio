9deebc16c16f2159f35a7d0689c56b226e483157
Syed Zaheed <139610883+szaid47@users.noreply.github.com>
2025-08-27 15:52:42 +0530

    resumeremoval

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -62,15 +62,7 @@ const Hero = () => {
             className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
               {HERO_CONTENT}
             </motion.p>
-            <motion.a
-            variants={childVariants}
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10  "
-            >
-              Download Resume
-            </motion.a>
+            
           </div>
           </motion.div>
         </div>
